9c4435634f27a50a7d29282ff7b5d1db675b1d36
lailaebrahim <ebrahimlaila815@gmail.com>
2024-10-14 21:28:33 +0300

    add urls for the user app

---
+++ NATURALILY/UserApp/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from . import views
+
+urlpatterns = [
+    path('register/', views.signUpView, name='signup'),
+    path('logout/', views.logOutView, name='logout'),
+    path('profile/<int:pk>', views.userProfileView, name='profile'),
+    path('<int:pk>/delete', views.deleteProfile, name='delete_profile'),
+    path('password-reset/', PasswordResetView.as_view(
+        template_name='UserApp/password_reset.html'), name='password_reset'),
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='UserApp/password_reset_done.html'),
+         name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>',
+         PasswordResetConfirmView.as_view(
+             template_name='UserApp/password_reset_confirm.html'),
+         name="password_reset_confirm"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name='UserApp/password_reset_complete.html'),
+         name="password_reset_complete"),
+]
